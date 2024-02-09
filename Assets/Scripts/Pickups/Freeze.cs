7786d5f7877221a371cf8092fcd59a7b1947f9b9
EloToJa <elotoja@protonmail.com>
2024-02-09 19:26:01 +0100

    Add Clock and Freeze

---
+++ Assets/Scripts/Pickups/Freeze.cs
@@ -0,0 +1,15 @@
+public class Freeze : Pickup
+{
+    public int freezeTime = 10;
+    
+    public override void Picked()
+    {
+        base.Picked();
+        GameManager.instance.FreezeTime(freezeTime);
+    }
+
+    private void Update()
+    {
+        Rotation();
+    }
+}
