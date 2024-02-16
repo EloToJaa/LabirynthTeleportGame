a774b4c3876368a282878edc9859369fb05564b0
EloToJa <elotoja@protonmail.com>
2024-02-16 19:17:56 +0100

    Update PortalTeleport

---
+++ Assets/Scripts/Portal/PortalTeleport.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform receiver;
+
+    private bool playerIsOverlapping = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+    private void Teleportation()
+    {
+        if(playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+            }
+        }
+    }
+}
