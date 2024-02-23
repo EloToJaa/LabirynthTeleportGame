039ef9f77a9ce510f3d2b0cde6ff329023aa06b7
EloToJa <elotoja@protonmail.com>
2024-02-23 19:07:38 +0100

    Update Portal.cs

--- Assets/Scripts/Portal/Portal.cs
+++ Assets/Scripts/Portal/Portal.cs
@@ -2,18 +2,30 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    public Transform portalCollider;
     public Portal otherPortal;
+    public Material material;
+
+    public Transform portalCollider;
+    private Transform renderSurface;
+    private Camera myCamera;
 
     private GameObject player;
     private PortalTeleport portalTeleport;
+    private PortalCamera portalCamera;
 
     private void Awake()
     {
+        myCamera = gameObject.transform.Find("PortalCamera").GetComponent<Camera>();
+        renderSurface = gameObject.transform.Find("RenderSurface");
+        portalCollider = gameObject.transform.Find("Collider");
+
         player = GameObject.FindGameObjectWithTag("Player");
 
         portalTeleport = portalCollider.GetComponent<PortalTeleport>();
         portalTeleport.player = player.transform;
         portalTeleport.receiver = otherPortal.portalCollider;
+
+        portalCamera = myCamera.GetComponent<PortalCamera>();
+
     }
 }
