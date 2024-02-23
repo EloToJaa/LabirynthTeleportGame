aebf4f24fc46b19e2a832b5516de19db86a554fb
EloToJa <elotoja@protonmail.com>
2024-02-23 18:59:11 +0100

    Update PortalCamera.cs

--- Assets/Scripts/Portal/PortalCamera.cs
+++ Assets/Scripts/Portal/PortalCamera.cs
@@ -17,5 +17,13 @@ public class PortalCamera : MonoBehaviour
         transform.position = portal.position + playerOffsetFromPortal;
 
         float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+
+        newCameraDirection = new Vector3(newCameraDirection.x * -1, newCameraDirection.y, newCameraDirection.z * -1);
+
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
     }
 }
