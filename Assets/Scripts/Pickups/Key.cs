01bbe50800c8b18339fa454b56840ec35b98a2d3
EloToJa <elotoja@protonmail.com>
2024-04-19 19:38:59 +0200

    Update Key.cs

--- Assets/Scripts/Pickups/Key.cs
+++ Assets/Scripts/Pickups/Key.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public enum KeyColor
 {
     Red,
@@ -9,6 +11,10 @@ public class Key : Pickup
 {
     public KeyColor keyColor;
 
+    public Material red;
+    public Material green;
+    public Material gold;
+
     public override void Picked()
     {
         base.Picked();
@@ -18,5 +24,26 @@ public class Key : Pickup
     private void Update()
     {
         Rotation();
+    }
+
+    private void Start()
+    {
+        SetMyColor();
+    }
+
+    private void SetMyColor()
+    {
+        switch (keyColor)
+        {
+            case KeyColor.Red:
+                GetComponent<Renderer>().material = red;
+                break;
+            case KeyColor.Green:
+                GetComponent<Renderer>().material = green;
+                break;
+            case KeyColor.Gold:
+                GetComponent<Renderer>().material = gold;
+                break;
+        }
     }
 }
