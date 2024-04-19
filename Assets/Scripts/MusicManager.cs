357eff79fc9f4424c22dd764ea58b84a015cc1ca
EloToJa <elotoja@protonmail.com>
2024-04-19 19:11:00 +0200

    Add MusicManager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] audioClips { get; private set; }
+
+    [SerializeField]
+    private int activeClipIndex = 0;
+
+    private AudioSource audioSource;
+    private double pauseClipTime = 0;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClips[activeClipIndex];
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if(audioSource.time >= audioClips[activeClipIndex].length)
+        {
+            activeClipIndex = (activeClipIndex + 1) % audioClips.Length;
+
+            audioSource.clip = audioClips[activeClipIndex];
+            audioSource.Play();
+        }
+    }
+}
